d8e4ff0f95ad34be64401a05ec27dbf6231feddf
Sergey T <tsa8@mail.ru>
2016-11-23 20:20:31 +0300

    second commit

--- db/migrate/20161116190832_create_floors.rb
+++ db/migrate/20161116190832_create_floors.rb
@@ -1,9 +1,8 @@
 class CreateFloors < ActiveRecord::Migration[5.0]
   def change
     create_table :floors do |t|
-      t.float :pos_x, :null => false
-      t.float :pos_y, :null => false
-      t.text :tags, array: true, default: []
+      t.float :pos_x,  array: true, default: [], :null => false
+      t.float :pos_y,  array: true, default: [], :null => false
       t.text :information
 
       t.timestamps
